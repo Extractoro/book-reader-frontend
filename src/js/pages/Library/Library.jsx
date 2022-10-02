4829af99a1953a6afbf677eaaa1c97c1f836b085
Vadym <vadymtytarenkoo@gmail.com>
2022-10-02 18:41:21 +0300

    library

--- src/js/pages/Library/Library.jsx
+++ src/js/pages/Library/Library.jsx
@@ -5,93 +5,99 @@ import Media from 'react-media';
 import sprite from '../../../sprites/library-sprite.svg';
 import s from './Library.module.css';
 import { Loading } from 'notiflix';
-import AlreadyReadList from 'js/components/AlreadyReadList'
-
+import AlreadyReadList from 'js/components/AlreadyReadList';
+import { useSelector } from 'react-redux';
+import { selectBooks } from 'js/redux/books/books-slice';
 
 const Library = () => {
-  const { data: allBooks, isFetching } = useFetchAllBooksQuery();
-// console.log(allBooks);
+  const { isFetching } = useFetchAllBooksQuery();
+  const books = useSelector(selectBooks);
+
   return (
     <div>
       <>
         {!isFetching && Loading.remove()}
         <LibraryForm />
 
-        {allBooks?.length > 0 ? <AlreadyReadList library={allBooks} /> : null}
-
-        <Media
-          query="(min-width: 768px)"
-          render={() => (
-            <div className={s['library-flex-container']}>
-              <div className={s['library-wrapper']}>
-                <div className={s['library-firstStep']}>
-                  <h4 className={s['library-stepsTitle']}>Крок 1.</h4>
-                  <div className={s['library-flex-wrapper']}>
-                    <div className={s['library-flex-cont']}>
-                      <svg
-                        className={s['library-main-icon']}
-                        width="22"
-                        height="18"
-                      >
-                        <use href={sprite + '#library-icon'} />
-                      </svg>
-                      <h5 className={s['library-stepsSuptitle']}>
-                        Створіть особисту бібліотеку
-                      </h5>
-                    </div>
-                    <div className={s['library-flex-wrapper']}>
-                      <div className={s['library-flex-cont']}>
-                        <svg
-                          className={s['library-arrow-icon']}
-                          width="10"
-                          height="12"
-                        >
-                          <use href={sprite + '#library-arrow'} />
-                        </svg>
-                        <p className={s['library-stepsParagrath']}>
-                          Додайте до неї книжки, які маєте намір прочитати.
-                        </p>
+        {books?.length > 0 ? (
+          <AlreadyReadList library={books} />
+        ) : (
+          <>
+            <Media
+              query="(min-width: 768px)"
+              render={() => (
+                <div className={s['library-flex-container']}>
+                  <div className={s['library-wrapper']}>
+                    <div className={s['library-firstStep']}>
+                      <h4 className={s['library-stepsTitle']}>Крок 1.</h4>
+                      <div className={s['library-flex-wrapper']}>
+                        <div className={s['library-flex-cont']}>
+                          <svg
+                            className={s['library-main-icon']}
+                            width="22"
+                            height="18"
+                          >
+                            <use href={sprite + '#library-icon'} />
+                          </svg>
+                          <h5 className={s['library-stepsSuptitle']}>
+                            Створіть особисту бібліотеку
+                          </h5>
+                        </div>
+                        <div className={s['library-flex-wrapper']}>
+                          <div className={s['library-flex-cont']}>
+                            <svg
+                              className={s['library-arrow-icon']}
+                              width="10"
+                              height="12"
+                            >
+                              <use href={sprite + '#library-arrow'} />
+                            </svg>
+                            <p className={s['library-stepsParagrath']}>
+                              Додайте до неї книжки, які маєте намір прочитати.
+                            </p>
+                          </div>
+                        </div>
+                        <div className={s['library-secondStep']}>
+                          <h4 className={s['library-stepsTitle']}>Крок 2.</h4>
+                          <div className={s['library-flex-wrapper']}>
+                            <div className={s['library-flex-cont']}>
+                              <svg
+                                className={s['library-main-icon']}
+                                width="22"
+                                height="18"
+                              >
+                                <use href={sprite + '#library-flag'} />
+                              </svg>
+                              <h5 className={s['library-stepsSuptitle']}>
+                                Сформуйте своє перше тренування
+                              </h5>
+                            </div>
+                            <div className={s['library-flex-wrapper']}>
+                              <div className={s['library-flex-cont']}>
+                                <svg
+                                  className={s['library-arrow-icon']}
+                                  width="10"
+                                  height="12"
+                                >
+                                  <use href={sprite + '#library-arrow'} />
+                                </svg>
+                                <p className={s['library-stepsParagrath']}>
+                                  Визначте ціль, оберіть період, розпочинайте
+                                  тренування.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className={s['library-secondStep']}>
-                  <h4 className={s['library-stepsTitle']}>Крок 2.</h4>
-                  <div className={s['library-flex-wrapper']}>
-                    <div className={s['library-flex-cont']}>
-                      <svg
-                        className={s['library-main-icon']}
-                        width="22"
-                        height="18"
-                      >
-                        <use href={sprite + '#library-flag'} />
-                      </svg>
-                      <h5 className={s['library-stepsSuptitle']}>
-                        Сформуйте своє перше тренування
-                      </h5>
-                    </div>
-                    <div className={s['library-flex-wrapper']}>
-                      <div className={s['library-flex-cont']}>
-                        <svg
-                          className={s['library-arrow-icon']}
-                          width="10"
-                          height="12"
-                        >
-                          <use href={sprite + '#library-arrow'} />
-                        </svg>
-                        <p className={s['library-stepsParagrath']}>
-                          Визначте ціль, оберіть період, розпочинайте
-                          тренування.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-        />
-        <Media query="(max-width: 767px)" render={() => <ModalLibrary />} />
+              )}
+            />
+            <Media query="(max-width: 767px)" render={() => <ModalLibrary />} />
+          </>
+        )}
       </>
       {isFetching && Loading.circle()}
     </div>
---
+++ src/js/redux/workout/workoutApi.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const workoutApi = createApi({
+  reducerPath: 'workoutApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://book-reader-backend.herokuapp.com',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['workout'],
+  endpoints: builder => ({
+    fetchAllWorkouts: builder.query({
+      query: () => ({ url: '/api/training' }),
+      providesTags: ['workout'],
+    }),
+    createWorkout: builder.mutation({
+      query: body => ({
+        url: '/api/training',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['workout'],
+    }),
+    updateWorkout: builder.mutation({
+      query: data => {
+        const { id, ...body } = data;
+        return {
+          url: `/api/training/${id}`,
+          method: 'PATCH',
+          body,
+        };
+      },
+      invalidatesTags: ['workout'],
+    }),
+  }),
+});
+
+export const {
+  useFetchAllWorkoutsQuery,
+  useCreateWorkoutMutation,
+  useUpdateWorkoutMutation,
+} = workoutApi;
